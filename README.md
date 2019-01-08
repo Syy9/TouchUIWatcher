01fb9df56b7245102d246a759325835bc3e1e812
appDeveloper <appdeveloper12345@yahoo.co.jp>
2019-01-09 00:46:33 +0900

    Write README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TouchUIWatcher
 Unity GameView Touch UI Watcher
+複雑なUI階層のUI探しに役立ちます。
+
+![demo](demo1.gif)
+
+# Usage 
+1. Open Window (Window/TouchUIWatcher)
+2. Play Scene
+3. Touch UI
+
+## License
+MIT
---
+++ TouchUIWatcher.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Syy.Tool
+{
+    public class TouchUIWatcher : EditorWindow
+    {
+        [MenuItem("Window/TouchUIWatcher")]
+        public static void Open()
+        {
+            GetWindow<TouchUIWatcher>("TouchUIWatcher");
+        }
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        Vector2 scrollPosition;
+
+        void OnEnable()
+        {
+           EditorApplication.update += Update;
+        }
+
+        void OnDisable()
+        {
+            EditorApplication.update -= Update;
+        }
+
+        void Update()
+        {
+            if (Application.isPlaying && (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)))
+            {
+                PointerEventData pointer = new PointerEventData(EventSystem.current);
+                pointer.position = Input.mousePosition;
+                EventSystem.current.RaycastAll(pointer, results);
+                scrollPosition = Vector2.zero;
+                if (Data.instance.highlight && results.Any())
+                {
+                    EditorGUIUtility.PingObject(results.First().gameObject);
+                }
+                Repaint();
+            }
+        }
+
+        void OnGUI()
+        {
+            if (!Application.isPlaying)
+            {
+                EditorGUILayout.LabelField("Playing only");
+                return;
+            }
+
+            Data.instance.highlight = EditorGUILayout.ToggleLeft($"Highlight     Hit {results.Count}", Data.instance.highlight);
+            if (results.Count > 0)
+            {
+                using (new EditorGUILayout.VerticalScope("box"))
+                {
+                    using (var scroll = new EditorGUILayout.ScrollViewScope(scrollPosition, false, true))
+                    {
+                        scrollPosition = scroll.scrollPosition;
+                        foreach (var result in results)
+                        {
+                            EditorGUILayout.ObjectField(result.gameObject, typeof(GameObject), true);
+                        }
+                    }
+                }
+            }
+        }
+
+        class Data : ScriptableSingleton<Data>
+        {
+            public bool highlight = true;
+        }
+    }
+}
